599784150166b70a6bc8ae3daa7afd59914d3b94
ThankGod Richard <madueketf@gmail.com>
2024-09-03 04:18:18 +0100

    Added home headers and views

---
+++ app/src/main/java/com/thangodr/shipmentappui/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.thangodr.shipmentappui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
+
+val APP_BLUE = Color(0xFF543b9c)
+val APP_ORANGE = Color(0xFFF7941D)
+val FIELD_GREY = Color(0XFFf8f8f8)
+val APP_WHite = Color(0xFFeae1fd)
+val APP_WHITE_BG = Color(0XFFFAFAFA)
+
+val Yellow = Color(0xFFF48525)
+val Placeholder_Grey = Color(0xFFb0b0b0)
